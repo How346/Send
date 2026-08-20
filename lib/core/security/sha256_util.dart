c67c788bcd5f08cda9185f72b36de08b5794e88f
How346 <How@pindush.click>
2026-08-20 17:40:43 +0530

    Refactor sha256File function for improved readability

--- lib/core/security/sha256_util.dart
+++ lib/core/security/sha256_util.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
+
 import 'package:crypto/crypto.dart';
 
-Future<String> sha256File(File file, {int chunkSize = 1024 * 1024}) async {
-  final digest = AccumulatorSink<Digest>();
-  final input = sha256.startChunkedConversion(digest);
-  final stream = file.openRead();
-  await for (final chunk in stream) {
-    input.add(chunk);
-  }
-  input.close();
-  return digest.events.single.toString();
+Future<String> sha256File(
+  File file, {
+  int chunkSize = 1024 * 1024,
+}) async {
+  final digest = await sha256.bind(
+    file.openRead(
+      0,
+      null,
+    ),
+  );
+
+  return digest.toString();
 }
