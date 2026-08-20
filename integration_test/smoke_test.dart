4f24e295ff59a5a128bb41498d115f8c6d6e8dfe
How346 <How@pindush.click>
2026-08-20 17:39:26 +0530

    Update import statement for HyperDrop app

--- integration_test/smoke_test.dart
+++ integration_test/smoke_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hyperdrop/main.dart';
+import 'package:hyperdrop/app.dart';
 
 void main() {
   testWidgets('HyperDrop starts', (tester) async {
     await tester.pumpWidget(const HyperDropApp());
+
     expect(find.text('HyperDrop'), findsOneWidget);
   });
 }
